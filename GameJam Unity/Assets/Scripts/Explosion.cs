18699c6628cbb08d4bd9488ec9c6c4c0d32b1e11
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-07-02 10:16:30 +0200

    Explosion
    Explosion prefab ready.

---
+++ GameJam Unity/Assets/Scripts/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int damage;
+    public float range;
+    public float speed;
+    public SphereCollider trigger;
+
+    private void Start()
+    {
+        trigger.radius = 0;
+    }
+
+    private void Update()
+    {
+        trigger.radius += speed;
+        if(trigger.radius >= range)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyUnit>().LoseHP(damage);
+        }
+    }
+}
