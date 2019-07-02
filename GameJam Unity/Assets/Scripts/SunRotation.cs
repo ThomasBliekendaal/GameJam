6d0b318a7743f6c63d1898a1bcc81c7556a6d2fa
ScottVetketel <47315956+ScottVetketel@users.noreply.github.com>
2019-07-02 20:40:35 +0200

    sun rotation

---
+++ GameJam Unity/Assets/Scripts/SunRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.RotateAroundLocal(gameObject.transform.right, rotationSpeed);
+    }
+}
