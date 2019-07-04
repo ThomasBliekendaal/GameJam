62d91f6e666cdaeae3dcc3626571366a6e9e55e1
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2019-07-04 13:04:11 +0200

    knightparticles

---
+++ GameJam Unity/Assets/Prefab/VFX/ParticleBaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBaker : MonoBehaviour
+{
+    public SkinnedMeshRenderer meshRender;
+    public Mesh mesh;
+    private bool updateMesh;
+    public float updateDelay;
+
+    public void UpdateMesh(bool shouldUpdateMesh)
+    {
+        updateMesh = shouldUpdateMesh;
+        if (shouldUpdateMesh)
+        {
+            StartCoroutine(UpdateMeshRenderer());
+        }
+    }
+    IEnumerator UpdateMeshRenderer()
+    {
+        while (updateMesh)
+        {
+            meshRender.BakeMesh(mesh);
+            yield return new WaitForSeconds(updateDelay);
+
+        }
+    }
+}
