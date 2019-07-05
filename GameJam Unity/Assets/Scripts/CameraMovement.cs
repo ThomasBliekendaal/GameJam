1b6b727590fa83169ce6cbf1aae9cf217307ba83
ScottVetketel <47315956+ScottVetketel@users.noreply.github.com>
2019-07-05 12:38:07 +0200

    camera

--- GameJam Unity/Assets/Scripts/CameraMovement.cs
+++ GameJam Unity/Assets/Scripts/CameraMovement.cs
@@ -6,8 +6,13 @@ public class CameraMovement : MonoBehaviour
 {
     public float maxPosRight;
     public float maxPosLeft;
+    public float trueZoom;
+    public float trueZoomMax = 1F;
+    public float trueZoomMin = 0F;
     public float zoomAmount;
-    public float moveSpeed = 1;
+    public float moveSpeed = 3;
+    public float moveSpeedFromStart = 3;
+    public float moveSpeedToAdd = 6;
     public float rotateSpeed = 1;
     public float hor;
     public float camHor;
@@ -19,48 +24,66 @@ public class CameraMovement : MonoBehaviour
 
     private void Start()
     {
-        //GetClampValues();
+        GetClampValues();
     }
 
     public void Update()
     {
         CamMovement();
-        CamZoom();
     }
 
     public void GetClampValues()
     {
-        camPos = gameObject.GetComponent<Transform>().position;
-        maxPosRight = camPos.x + 20.5F;
-        maxPosLeft = camPos.x - 20.5F;
+        camPos.x = -11.06592F;
+        maxPosRight = camPos.x + 5.5F;
+        maxPosLeft = camPos.x - 5.5F;
     }
 
     public void CamMovement()
     {
         //movement button
-        hor = Input.GetAxis("Horizontal");
-        zoomAmount = Input.GetAxis("Mouse ScrollWheel");
-
-        //set axis
-        camPos.x = hor;
+        camPos.x = Input.GetAxis("Horizontal");
 
         //set movement to transform and equalize the speed
-        transform.Translate(camPos * Time.deltaTime * moveSpeed);
+        gameObject.transform.Translate(camPos * Time.deltaTime * moveSpeed);
+        CameraZoomPosition1.transform.Translate(camPos * Time.deltaTime * moveSpeed);
+        CameraZoomPosition2.transform.Translate(camPos * Time.deltaTime * moveSpeed);
+
+        //Zoom
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            zoomAmount = Input.GetAxis("Mouse ScrollWheel");
+            trueZoom = trueZoom + zoomAmount;
+            CamZoom();
+        }
+
+        //speed up camera move speed
+        if (Input.GetButtonDown("SpeedCamera"))
+        {
+            moveSpeed = moveSpeed + moveSpeedToAdd;
+        }
+        if (Input.GetButtonUp("SpeedCamera"))
+        {
+            moveSpeed = moveSpeedFromStart;
+        }
+
+        //clamp the true zoom amount
+        if(trueZoom >= trueZoomMax)
+        {
+            trueZoom = trueZoomMax;
+        }
+
+        if (trueZoom <= trueZoomMin)
+        {
+            trueZoom = trueZoomMin;
+        }
 
         //clamp the camera positions
-        //if(camPos.x > maxPosLeft)
-        //{
-        //    camPos.x = maxPosLeft;
-        //}
-
-        //if (camPos.x > maxPosRight)
-        //{
-        //   camPos.x = maxPosRight;
-        //}
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxPosLeft, maxPosRight), transform.position.y, transform.position.z);
     }
 
     public void CamZoom()
     {
-        transform.position = Vector3.Lerp(CameraZoomPosition1.GetComponent<Transform>().position, CameraZoomPosition2.GetComponent<Transform>().position, zoomAmount);
+        transform.position = Vector3.Lerp(CameraZoomPosition1.GetComponent<Transform>().position, CameraZoomPosition2.GetComponent<Transform>().position, trueZoom);
     }
 }
